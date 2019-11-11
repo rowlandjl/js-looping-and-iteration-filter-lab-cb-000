5ace417f52f4d6a47960214cf8b7befa4990b73c
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-11 14:14:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function fuzzyMatch(drivers, str) {
 
 function matchName(drivers, str) {
   return drivers.filter(function(driver) {
-    driver.name.toLowerCase() === str.toLowerCase();
+    return driver.name.toLowerCase() === str.toLowerCase();
   });
 }
